b36161b3623e04481ec8295bfc5eb41f7492fef0
jimmy-bellinghausen <jimmy.bellinghausen@gmail.com>
2020-03-31 20:20:31 -0500

    postMovie and getAllMovies controller tests passing

--- src/main/java/com/galvanize/controllers/MovieController.java
+++ src/main/java/com/galvanize/controllers/MovieController.java
@@ -1,8 +1,10 @@
 package com.galvanize.controllers;
 
+import com.galvanize.entities.Movie;
 import com.galvanize.services.MovieService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/movie")
@@ -12,4 +14,15 @@ public class MovieController {
     public MovieController(MovieService service) {
         this.service = service;
     }
+
+    @PostMapping
+    public Movie postMovie(@RequestBody Movie input){
+        return service.postMovie(input);
+    }
+
+    @GetMapping
+    public List<Movie> getAllMovies(){
+        return service.getAllMovies();
+    }
+
 }
